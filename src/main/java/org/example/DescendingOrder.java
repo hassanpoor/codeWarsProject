60ffb650eb46cd77cb502d702bf91fe16334c049
en.hassanpoor <en.hassanpoor@gmail.com>
2023-08-13 18:58:34 +0430

    first commit

--- src/main/java/org/example/DescendingOrder.java
+++ src/main/java/org/example/DescendingOrder.java
@@ -1,9 +1,5 @@
 package org.example;
 
-import java.lang.reflect.Array;
-import java.util.Arrays;
-import java.util.List;
-
 public class DescendingOrder {
     public static int sortDesc(final int num) {
         return 0;
